875ee85b753e884c43f682fee0c89a33945b0ea2
LemonLim18 <limmiinning@gmail.com>
2023-12-25 02:58:29 +0800

    Fix bugs

--- compilation.py
+++ compilation.py
@@ -1,7 +1,7 @@
 import random
 
 # START OF SCAN
-def SCAN(arr, head, disk_size):
+def SCAN(requestArray, headPosition, cylinderNum):
  
     seek_count = 0
     max_seek = 0
@@ -10,76 +10,62 @@ def SCAN(arr, head, disk_size):
     right = []
     seek_sequence = []
  
-    # Appending end values
-    # which has to be visited
-    # before reversing the direction
-    right.append(disk_size - 1)
+    right.append(cylinderNum - 1)
  
     for i in range(size):
-        if (arr[i] < head):
-            left.append(arr[i])
-        if (arr[i] > head):
-            right.append(arr[i])
+        if (requestArray[i] < headPosition):
+            left.append(requestArray[i])
+        if (requestArray[i] > headPosition):
+            right.append(requestArray[i])
  
-    # Sorting left and right vectors
+
     left.sort()
     right.sort()
  
-    # Scanning right
+
     for i in range(len(right)):
         cur_track = right[i]
              
-        # Appending current track to seek 
-        # sequence
         seek_sequence.append(cur_track)
  
-        # Calculate absolute distance
-        distance = abs(cur_track - head)
+        distance = abs(cur_track - headPosition)
  
-        # Increase the total count
         seek_count += distance
 
-        # Check if this distance is the max seek
         max_seek = max(max_seek, distance)
  
-        # Accessed track is now new head
-        head = cur_track
+        headPosition = cur_track
          
     # Scanning left
     for i in range(len(left) - 1, -1, -1):
         cur_track = left[i]
  
-        # Appending current track to 
-        # seek sequence
         seek_sequence.append(cur_track)
  
         # Calculate absolute distance
-        distance = abs(cur_track - head)
+        distance = abs(cur_track - headPosition)
  
-        # Increase the total count
         seek_count += distance
 
-        # Check if this distance is the max seek
         max_seek = max(max_seek, distance)
  
-        # Accessed track is now the new head
-        head = cur_track
+        headPosition = cur_track
     
-    print('SCAN: \n')
-    print("Total number of seek operations =", 
+    print('SCAN:')
+    print("\tTotal number of seek operations =", 
           seek_count)
  
-    print("Average seek time =", seek_count / size)
+    print("\tAverage seek time =", seek_count / size)
 
-    print("Worst-case seek time =", max_seek)
+    print("\tWorst-case seek time =", max_seek)
  
-    print("Seek Sequence is: ", end="")
-    print(*seek_sequence, sep=", ")
+    print("\tSeek sequence: ", end="")
+    print("\t" + ", ".join(map(str, seek_sequence)))
 # END OF SCAN
  
 
 # START OF CSCAN
-def CSCAN(arr, head, disk_size):
+def CSCAN(requestArray, headPosition, cylinderNum):
  
     seek_count = 0
     max_seek = 0
@@ -88,86 +74,66 @@ def CSCAN(arr, head, disk_size):
     right = []
     seek_sequence = []
  
-    # Appending end values
-    # which has to be visited5
-    # before reversing the direction
     left.append(0)
-    right.append(disk_size - 1)
+    right.append(cylinderNum - 1)
  
     for i in range(size):
-        if (arr[i] < head):
-            left.append(arr[i])
-        if (arr[i] > head):
-            right.append(arr[i])
+        if (requestArray[i] < headPosition):
+            left.append(requestArray[i])
+        if (requestArray[i] > headPosition):
+            right.append(requestArray[i])
  
-    # Sorting left and right vectors
     left.sort()
     right.sort()
  
-    # First service the requests
-    # on the right side of the
-    # head.
     for i in range(len(right)):
         cur_track = right[i]
  
-        # Appending current track
-        # to seek sequence
         seek_sequence.append(cur_track)
  
-        # Calculate absolute distance
-        distance = abs(cur_track - head)
+        distance = abs(cur_track - headPosition)
  
-        # Increase the total count
         seek_count += distance
 
-        # Check if this distance is the max seek
         max_seek = max(max_seek, distance)
  
-        # Accessed track is now new head
-        head = cur_track
+        headPosition = cur_track
  
-    # Once reached the right end
-    # jump to the beginning.
-    head = 0
+
+    headPosition = 0
  
-    # adding seek count for head returning from 199 to 0
-    seek_count += (disk_size - 1)
+    seek_count += (cylinderNum - 1)
  
-    # Now service the requests again
-    # which are left.
+
     for i in range(len(left)):
         cur_track = left[i]
  
-        # Appending current track
-        # to seek sequence
+
         seek_sequence.append(cur_track)
  
-        # Calculate absolute distance
-        distance = abs(cur_track - head)
+        distance = abs(cur_track - headPosition)
  
-        # Increase the total count
         seek_count += distance
 
-        # Check if this distance is the max seek
         max_seek = max(max_seek, distance)
  
-        # Accessed track is now the new head
-        head = cur_track
+
+        headPosition = cur_track
     
-    print('C-SCAN: \n')
-    print("Total number of seek operations =", 
+    print('\n\nC-SCAN:')
+    print("\tTotal number of seek operations =", 
           seek_count)
  
-    print("Average seek time =", seek_count / size)
+    print("\tAverage seek time =", seek_count / size)
 
-    print("Worst-case seek time =", max_seek)
+    print("\tWorst-case seek time =", max_seek)
  
-    print("Seek Sequence is: ", end="")
-    print(*seek_sequence, sep=", ")
+    print("\tSeek sequence: ", end="")
+    print("\t" + ", ".join(map(str, seek_sequence)))
 # END OF CSCAN
 
 # START OF CLOOK
-def CLOOK(arr, head):
+def CLOOK(requestArray, headPosition):
  
     seek_count = 0
     max_seek = 0
@@ -176,48 +142,34 @@ def CLOOK(arr, head):
     right = []
     seek_sequence = []
  
-    for i in range(len(arr)):
-        if (arr[i] < head):
-            left.append(arr[i])
-        if (arr[i] > head):
-            right.append(arr[i])
+    for i in range(len(requestArray)):
+        if (requestArray[i] < headPosition):
+            left.append(requestArray[i])
+        if (requestArray[i] > headPosition):
+            right.append(requestArray[i])
  
-    # Sorting left and right vectors
+
     left.sort()
     right.sort()
  
-    # First service the requests
-    # on the right side of the
-    # head
+
     for i in range(len(right)):
         cur_track = right[i]
  
-        # Appending current track
-        # to seek sequence
         seek_sequence.append(cur_track)
  
-        # Calculate absolute distance
-        distance = abs(cur_track - head)
+        distance = abs(cur_track - headPosition)
  
-        # Increase the total count
         seek_count += distance
 
-        # Check if this distance is the max seek
         max_seek = max(max_seek, distance)
  
-        # Accessed track is now new head
-        head = cur_track
+        headPosition = cur_track
  
-    # Once reached the right end
-    # jump to the nearest track that
-    # is needed to be serviced in
-    # left direction
     if len(left) != 0:
-        seek_count += abs(head - left[0])
-        head = left[0]
- 
-    # Now service the requests again
-    # which are left
+        seek_count += abs(headPosition - left[0])
+        headPosition = left[0]
+
     for i in range(len(left)):
         cur_track = left[i]
  
@@ -226,7 +178,7 @@ def CLOOK(arr, head):
         seek_sequence.append(cur_track)
  
         # Calculate absolute distance
-        distance = abs(cur_track - head)
+        distance = abs(cur_track - headPosition)
  
         # Increase the total count
         seek_count += distance
@@ -234,35 +186,37 @@ def CLOOK(arr, head):
         # Check if this distance is the max seek
         max_seek = max(max_seek, distance)
  
-        # Accessed track is now the new head
-        head = cur_track
+        # Accessed track is now the new headPosition
+        headPosition = cur_track
     
-    print('C-LOOK: \n')
-    print("Total number of seek operations =", 
+    print('\n\nC-LOOK:')
+    print("\tTotal number of seek operations =", 
           seek_count)
  
-    print("Average seek time =", seek_count / len(arr))
+    print("\tAverage seek time =", seek_count / len(requestArray))
 
-    print("Worst-case seek time =", max_seek)
+    print("\tWorst-case seek time =", max_seek)
  
-    print("Seek Sequence is: ", end="")
-    print(*seek_sequence, sep=", ")
-    print('\n')
+    print("\tSeek sequence: ", end="")
+    # to prevent the first comma to appear before the first element
+    print("\t" + ", ".join(map(str, seek_sequence)))
+    print('\n\n')
 # END OF CLOOK
 
-# Driver code
-head = 50
-disk_size = 200
+# Parameter value
+print('\n\n')
+headPosition = 50
+cylinderNum = 200
 size = int(input("Enter the number of I/O requests: "))
 # size = 8
-arr = random.sample(range(disk_size), size)
+requestArray = random.sample(range(cylinderNum), size)
 
-# For testing purposes, use the following array:
-arr = [ 176, 79, 34, 60, 92, 11, 41, 114 ]
-print("Array generated: ", end="")
-print(*arr, sep=", ")
+# For testing purposes, use the following requestArrayay:
+# requestArray = [ 176, 79, 34, 60, 92, 11, 41, 114 ]
+print("Random Request Array generated: ", end="")
+print(", ".join(map(str, requestArray)))
 print('\n')
 
-SCAN(arr, head, disk_size)
-CSCAN(arr, head, disk_size)
-CLOOK(arr, head)
+SCAN(requestArray, headPosition, cylinderNum)
+CSCAN(requestArray, headPosition, cylinderNum)
+CLOOK(requestArray, headPosition)
